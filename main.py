341c5c8446182d489e43e4f48eebb0a0f4fc99f9
LouisM <louismillot1@msn.com>
2019-07-17 22:39:45 +0200

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+if __name__ == "__main__":
+    # Load Yolo
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    print("ok")
+    print("end")
